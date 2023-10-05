111f199ec3da20931ec6344acaf8ccb30229846b
Ajay Sharma <sharmaajay9813@gmail.com>
2023-10-05 19:26:20 +0530

    Model trainer

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from src.DSProject.exception import CustomException
 from src.DSProject.components.data_ingestion import DataIngestion
 # from src.DSProject.components.data_ingestion import DataIngestionConfig
 from src.DSProject.components.data_transformation import DataTransformationConfig,DataTransformation 
+from src.DSProject.components.model_trainer import ModelTrainer, ModelTrainerConfig
 import sys 
 
 if __name__=="__main__":
@@ -16,7 +17,13 @@ if __name__=="__main__":
 
         # data_transformation_config=DataTransformationConfig()
         data_transformation=DataTransformation()
-        data_transformation.initiate_data_transformation(train_data_path,test_data_path)
+        train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data_path,test_data_path)
+
+
+        #Model Training
+        model_trainer = ModelTrainer()
+        print(model_trainer.initiate_model_trainer(train_arr,test_arr))
+
         
     except Exception as e:
         logging.info("Custom Exception")
