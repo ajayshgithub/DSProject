853d55f2b803d8890ca8d7d04c0363cae7f81a7f
Ajay Sharma <sharmaajay9813@gmail.com>
2023-10-24 13:25:18 +0530

    MOdel Training and Hyperparameter Tuning Done

--- src/DSProject/components/model_trainer.py
+++ src/DSProject/components/model_trainer.py
@@ -97,7 +97,7 @@ class ModelTrainer:
 
             if best_model_score<0.6:
                 raise CustomException("No best model found")
-            logging.info(f"Best found model on boht training and testing dataset")
+            logging.info(f"Best found model on both training and testing dataset")
 
             save_object(
                 file_path=self.model_trainer_config.trained_model_file_path,
@@ -108,8 +108,6 @@ class ModelTrainer:
 
             r2_square = r2_score(y_test,predicted)
             return r2_square
-
-
 
         except Exception as e:
             raise CustomException(e,sys)
